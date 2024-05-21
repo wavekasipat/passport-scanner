e856fc48364df64cb9edefe343b94f387d0ec49d
wavekasipat <wavekasipat@gmail.com>
2024-05-21 18:16:59 +0700

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,59 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Button, Pressable } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+import { forwardRef } from "react";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+            headerImage={
+                <Image
+                    source={require("@/assets/images/partial-react-logo.png")}
+                    style={styles.reactLogo}
+                />
+            }
+        >
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Welcome!</ThemedText>
+                <HelloWave />
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Scan a Passport</ThemedText>
+                <Link href="/passport" asChild>
+                    <MyButton title="Open Camera" />
+                    {/* <Pressable>
+                        <ThemedText>Home</ThemedText>
+                    </Pressable> */}
+                </Link>
+            </ThemedView>
+        </ParallaxScrollView>
+    );
 }
 
+const MyButton = forwardRef(({ title, ...props }: { title: string }, ref) => {
+    return <Button {...props} title={title} />;
+});
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    titleContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: "absolute",
+    },
 });
---
+++ app/passport.tsx
@@ -0,0 +1,150 @@
+import {
+    Image,
+    StyleSheet,
+    Platform,
+    Button,
+    View,
+    TouchableOpacity,
+    Dimensions,
+} from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import React from "react";
+
+export default function PassportScreen() {
+    const [permission, requestPermission] = useCameraPermissions();
+    const [cameraRef, setCameraRef] = React.useState<CameraView | null>(null);
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.container}>
+                <ThemedText>
+                    We need your permission to show the camera
+                </ThemedText>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    const takePicture = async () => {
+        if (cameraRef) {
+            // Take the picture
+            const photo = await cameraRef.takePictureAsync();
+            console.log(photo);
+        }
+    };
+
+    return (
+        <ThemedView style={{ flex: 1 }}>
+            <CameraView
+                style={styles.camera}
+                facing={"back"}
+                ref={(ref) => {
+                    // Save a reference to the camera view
+                    setCameraRef(ref);
+                }}
+            >
+                <View style={styles.passportOverlay} />
+                <View style={styles.pictureOverlay} />
+                <View style={styles.mrzOverlay} />
+                <TouchableOpacity
+                    style={styles.snapButtonContainer}
+                    onPress={takePicture}
+                >
+                    <View style={styles.snapButton}></View>
+                </TouchableOpacity>
+            </CameraView>
+        </ThemedView>
+    );
+}
+
+const { width, height } = Dimensions.get("window");
+const passportHeight = width * 0.9;
+const passportWidth = (passportHeight * 125) / 88;
+const pictureHeight = (passportHeight * 45) / 88;
+const pictureWidth = (passportWidth * 35) / 125;
+const mrzWidth = passportWidth
+const mrzHeight = (passportHeight * 23.2) / 88;
+
+const passportPositionTop = (height - passportWidth) / 2.5;
+const passportPositionLeft = (width - passportHeight) / 2;
+
+const pictureMarginLeft = (passportWidth * 2) / 125;
+const pictureMarginBottom = (passportHeight * 25.2) / 88;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    camera: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        padding: 24,
+    },
+    passportOverlay: {
+        position: "absolute",
+        left: passportPositionLeft,
+        top: passportPositionTop,
+        width: passportHeight,
+        height: passportWidth,
+        borderWidth: 2,
+        borderColor: "white",
+        borderRadius: 8,
+        borderTopLeftRadius: 16,
+        borderBottomLeftRadius: 16,
+    },
+    mrzOverlay: {
+        position: "absolute",
+        left: passportPositionLeft,
+        top: passportPositionTop,
+        width: mrzHeight,
+        height: mrzWidth,
+        borderWidth: 2,
+        borderColor: "white",
+        borderRadius: 0,
+        borderTopLeftRadius: 16,
+        borderBottomLeftRadius: 16,
+    },
+    pictureOverlay: {
+        position: "absolute",
+        left: passportPositionLeft+pictureMarginBottom,
+        top: passportPositionTop + pictureMarginLeft,
+        width: pictureHeight,
+        height: pictureWidth,
+        borderWidth: 2,
+        borderColor: "white",
+        borderRadius: 8,
+    },
+    snapButtonContainer: {
+        backgroundColor: "white",
+        borderRadius: 52,
+        height: 52,
+        width: 52,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    snapButton: {
+        // soft red circle
+        backgroundColor: "rgba(255, 0, 0, 0.5)",
+        borderRadius: 50,
+        height: 50,
+        width: 50,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+    },
+});
